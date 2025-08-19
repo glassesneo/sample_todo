4853af320157892a39026c5eddfb61bf8ba6a95d
neo <glassesneo@protonmail.com>
2025-08-19 15:41:16 +0900

    feat: improve design

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -4,7 +4,11 @@ import { Todo } from "./types.ts";
 export const TodoList = () => {
   const [todoList, setTodoList] = useState<Todo[]>([]);
   const todoElements = todoList.map((todo) => (
-    <li key={todo.id}>{todo.title}</li>
+    <li key={todo.id} className="list-row">
+      <div>
+        {todo.title}
+      </div>
+    </li>
   ));
   const [todoTitle, setTodoTitle] = useState("");
   let id: number = 0;
@@ -15,24 +19,30 @@ export const TodoList = () => {
     return todo;
   };
   return (
-    <div className="m-4">
-      <ul className="list bg-base-100 rounded-box shadow-md">
+    <div className="m-8">
+      <ul className="list bg-base-100 max-w-md mx-auto rounded-box shadow-md">
+        <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
+          Todo list
+        </li>
         {todoElements}
+        <li className="join p-4">
+          <label className="input join-item">
+            <input
+              type="text"
+              value={todoTitle}
+              onChange={(e) => setTodoTitle(e.target.value)}
+            />
+          </label>
+          <button
+            className="btn btn-accent join-item"
+            type="button"
+            disabled={todoTitle === ""}
+            onClick={() => setTodoList([...todoList, newTodo(todoTitle)])}
+          >
+            Add
+          </button>
+        </li>
       </ul>
-
-      <input
-        type="text"
-        value={todoTitle}
-        onChange={(e) => setTodoTitle(e.target.value)}
-      />
-      <button
-        className="btn btn-primary m-4"
-        type="button"
-        disabled={todoTitle === ""}
-        onClick={() => setTodoList([...todoList, newTodo(todoTitle)])}
-      >
-        Add
-      </button>
     </div>
   );
 };
